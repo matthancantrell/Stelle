dac062e1eb213b8f358efab77b9f41466725960d
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-10-04 19:56:23 -0600

    Reworked Directory

--- Stelle_Base/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Stelle_Base;
-
-Console.WriteLine("Hello, World!");
-UserSystemInteraction usi = new UserSystemInteraction();
-usi.Start();
