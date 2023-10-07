0feb6e019b98ce1c2432e30be8c07e7b171f9862
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-10-06 18:52:59 -0600

    Webview Alteration
    - Updated Stelle's Webview Panel!
    - Added an image
    -Made it appear on the right side of the screen
    - Made a textbox for data entry

--- stelle/src/extension.ts
+++ stelle/src/extension.ts
@@ -34,11 +34,30 @@ export function activate(context: vscode.ExtensionContext) {
 			const panel = vscode.window.createWebviewPanel(
 				'stelle', // Identifies type of webview. Internal use.
 				'Stelle', // Title of the panel. Displayed to user
-				vscode.ViewColumn.One, // Editor Column to show the new webview panel within
+				vscode.ViewColumn.Two, // Editor Column to show the new webview panel within
 				{} // Webview options go here
 			);
+
+			panel.webview.html = getWebviewContent();
 		})
 	);
+}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cat Coding</title>
+</head>
+<body>
+	<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	<form>
+		<input type="text" id="fname" name="fname"
+	<form/>
+</body>
+</html>`;
 }
 
 // This method is called when your extension is deactivated
