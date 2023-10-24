af75a31baf136ed7c0cd63a62a569e20dd905b65
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-10-24 16:03:09 -0600

    Updated. Working as is

--- stelle/src/webview.ts
+++ stelle/src/webview.ts
@@ -10,8 +10,13 @@ export function getWebviewContent() {
 
 		/* Define a CSS class to add a border */
 		.bordered {
-			border: 2px solid black; /* You can customize the border style, color, and width */
-			padding: 10px; /* Add padding for spacing */
+			border: 2px solid white;
+			padding: 10px;
+			background: gray;
+		}
+
+		textarea { 
+			border-radius: 10px;
 		}
 		
 		.product-font {
@@ -27,16 +32,24 @@ export function getWebviewContent() {
 			font-size: 2em;
 		}
 
+		.medium-text {
+			font-size: 2em;
+		}
+
 		.non-resizeable {
 			resize: none;
 			width: 90%
 		}
+
+		.large-button { padding: 20px; }
 
 		.centered {
 			text-align: center;
 			display: block;
 			margin: 0 auto; /* Center the element horizontally on the page */
 		}
+
+		body { color: white; background-color: black; }
 
 		.center-text { text-align: center; }
 		</style>
@@ -114,17 +127,21 @@ export function getWebviewContent() {
 		</script>
 	</head>
 	<body>
-		<!-- <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" /> -->
-		<br>
-		<div class="bordered">
-			<p id="code"></p>
+		<br><br>
+		<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" class="centered" />
+		<br><br>
+		<div class="bordered" style="border-radius: 10px;">
+			<p id="code" style="font-size:1.25em; text-align: center"></p>
 		</div>
-		<p id="R" class="product-font centered">
+		<br><br>
+		<p id="R" class="product-font centered large-text">
 			Hello! I am Stelle, an AI assistant built to help you learn to code! Ask me any code related questions and I will do my best to help!
 		</p>
+		<br><br>
 		<br>
 		<form id="userInput" method="POST">
-			<textarea type="text" id="userText" class="non-resizeable centered" placeholder="Talk to Stelle here!" oninput="autoResize(this)"></textarea>
+			<textarea type="text" id="userText" class="non-resizeable centered product-font" placeholder="Talk to Stelle here!" oninput="autoResize(this)" style="font-size: 2em;"></textarea>
+			<br>
 			<button type="button" onclick="submitUserData()" id="submitUserText" class="centered">Submit</button>
 		</form>	
 	</body>
